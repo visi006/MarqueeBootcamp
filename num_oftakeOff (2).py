370c2a73458b741dbaa460de322fe5abd763f34d
Vismay Pradeep <167422286+visi006@users.noreply.github.com>
2026-01-02 21:29:22 +0530

    Add comments for clarity on airline schedules
    Added comments to clarify the purpose of variables and logic.

--- num_oftakeOff (2).py
+++ num_oftakeOff (2).py
@@ -1,3 +1,18 @@
+#hints for the code
+
+#nod → Number of days
+
+#f1, f2, f3 → Three airlines
+
+#Each airline has a fixed schedule:
+
+#Airline 1 flies every f1 days
+
+#Airline 2 flies every f2 days
+
+#Airline 3 flies every f3 days
+
+
 nod = int(input())
 f1 = int(input())
 f2 = int(input())
@@ -10,3 +25,4 @@ for day in range ( 1, nod+1):
     if day % f3 == 0 : divCtr+=1
     if divCtr == 1: res+=1
 print(res)
+
