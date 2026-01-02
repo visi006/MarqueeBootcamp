3dc92a57380462b428d3ca6a64e61f31bd083f62
Vismay Pradeep <167422286+visi006@users.noreply.github.com>
2026-01-02 22:00:16 +0530

    Implement rainwater trapping algorithm in Java

--- 01.01.2026/rainwatertrap.java
+++ 01.01.2026/rainwatertrap.java
@@ -40,5 +40,29 @@ class Solution:
         return ans
 
   Java Code :
+
+class Solution {
+    public int trap(int[] height) {
+        int n = height.length;
+        int l = 0, r = n - 1;
+        int lmax = 0, rmax = 0;
+        int ans = 0;
+
+        while (l < r) {
+            lmax = Math.max(lmax, height[l]);
+            rmax = Math.max(rmax, height[r]);
+
+            if (lmax < rmax) {
+                ans += lmax - height[l];
+                l++;
+            } else {
+                ans += rmax - height[r];
+                r--;
+            }
+        }
+        return ans;
+    }
+}
+  
   
 
