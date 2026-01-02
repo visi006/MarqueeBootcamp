4d746a540fa2f3556e6957eafcfb1a123e04080d
Vismay Pradeep <167422286+visi006@users.noreply.github.com>
2026-01-02 21:33:15 +0530

    Enhance code readability with comments
    Added comments to clarify the code's purpose and logic.

--- num_oftakeOff (2).py
+++ num_oftakeOff (2).py
@@ -17,13 +17,14 @@ nod = int(input())   # Takes input values
 f1 = int(input())    #Total number of days  and Three flight intervals
 f2 = int(input())     #Total number of days  and Three flight intervals
 f3 = int(input())     #Total number of days  and Three flight intervals
-res=0
-for day in range ( 1, nod+1):
+res=0                  #res stores the number of days where exactly one flight takes off
+for day in range ( 1, nod+1):    #Checks each day from Day 1 to Day N
     divCtr=0
-    if day % f1 == 0 : divCtr+=1
-    if day % f2 == 0 : divCtr+=1
-    if day % f3 == 0 : divCtr+=1
-    if divCtr == 1: res+=1
-print(res)
+    if day % f1 == 0 : divCtr+=1   #Airline 1 flies if day is divisible by f1
+    if day % f2 == 0 : divCtr+=1   # Airline 2 flies if day is divisible by f2
+    if day % f3 == 0 : divCtr+=1   #Airline 3 flies if day is divisible by f3
+    if divCtr == 1: res+=1         #Only count the day if exactly one airline is flying
+print(res)                          # print the total count of res values 
+
 
 
