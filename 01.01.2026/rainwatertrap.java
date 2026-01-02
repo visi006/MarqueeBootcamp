7608cca3362c0f856ba234fe556779154fec3544
Vismay Pradeep <167422286+visi006@users.noreply.github.com>
2026-01-02 21:59:33 +0530

    Update rainwatertrap.java

--- 01.01.2026/rainwatertrap.java
+++ 01.01.2026/rainwatertrap.java
@@ -1,1 +1,43 @@
+Problem Statement:
+  Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+Refer Leetcode Problem 42
+
+Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+Output: 6
+Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped.
+Example 2:
+
+Input: height = [4,2,0,3,2,5]
+Output: 9
+ 
+
+Constraints:
+
+n == height.length
+1 <= n <= 2 * 104
+0 <= height[i] <= 105
+
+  Python Code :
+
+class Solution:
+    def trap(self, height):
+        n = len(height)
+        l, r = 0, n - 1
+        lmax = rmax = 0
+        ans = 0
+
+        while l < r:
+            lmax = max(lmax, height[l])
+            rmax = max(rmax, height[r])
+
+            if lmax < rmax:
+                ans += lmax - height[l]
+                l += 1
+            else:
+                ans += rmax - height[r]
+                r -= 1
+
+        return ans
+
+  
 
