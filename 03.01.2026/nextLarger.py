6be1679e87ae62ab6a6f4233e9c129cea41c030c
Vismay Pradeep <167422286+visi006@users.noreply.github.com>
2026-01-03 21:32:04 +0530

    Add next larger number algorithm in nextLarger.py
    Implement logic to find the next larger number from the given digits.

--- 03.01.2026/nextLarger.py
+++ 03.01.2026/nextLarger.py
@@ -6,3 +6,30 @@ Output
 [2763,961235,-1,   54855446]
 
 Python code for the given problem statement :
+
+num = int(input())
+copy_num = num
+num_list=[]
+while copy_num!=0:
+    digit = copy_num %10
+    num_list.insert(0,digit)
+    copy_num//=10
+
+print(num_list)    
+for iter in range(len(num_list)-1,-1,-1):
+    if num_list[iter-1] < num_list[iter]:
+        break
+digit = num_list[iter-1]    
+large = num_list[iter]
+large_ind = iter
+for walk in range ( iter, len(num_list)-1):
+    if num_list[walk] > digit and num_list[walk] < large:
+        large = num_list [ walk ]
+        large_ind = walk
+temp = num_list [ iter - 1]
+num_list [ iter - 1] = num_list [ large_ind]
+num_list [ large_ind ] = temp
+
+print(num_list)
+
+#    5456321
