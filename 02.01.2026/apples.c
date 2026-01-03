698a5247e33c0535756a627dfa342b9fe633ba56
Vismay Pradeep <167422286+visi006@users.noreply.github.com>
2026-01-03 21:38:56 +0530

    Add problem statement for moving apples
    Added problem statement and example for moving apples between baskets to equalize counts.

--- 02.01.2026/apples.c
+++ 02.01.2026/apples.c
@@ -1,4 +1,22 @@
+Problem statement: Moving Apples
+There are N number of baskets, where the basket contains  apples.
+We want to move apples between baskets so that all baskets have the same number of apples. What is the minimum number of apples that must be moved?
+It is guaranteed that there exists a way to move apples so as to have an equal number of apples in each basket
+Input Specification:
+inpu t: N, denoting the Number of Baskets
+input2: array representing the number of apples in the ith basket
+Output Specification:
+Return the minimum number of apples that must be moved so that all baskets have the same number of apples
+Example 1:
+input1: 2
+input2: (1,3)
+Output: 1
+Explanation:
+There are 2 baskets with first contaming 1 apple and second containing 3 apples
+If we shift one apple from the second container to the first container then both will have 2 apples So only one apple was required to move Hence the output is 1
 
+7
+10 8 12  7 10 4  5
 
 
 
