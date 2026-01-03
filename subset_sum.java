364b5831a4b454071fbca3f1fa15d6a23316e744
Vismay Pradeep <167422286+visi006@users.noreply.github.com>
2026-01-03 21:24:58 +0530

    Update subset_sum.java

--- subset_sum.java
+++ subset_sum.java
@@ -1,3 +1,19 @@
+Subset Sum
+Given a set of non-negative integers and a value sum, the task is to check if there is a subset of the given set whose sum is equal to the given sum.
+
+Examples:
+Input: set[] = {3, 34, 4, 12, 5, 2}, sum = 9
+Output: True
+Explanation: There is a subset (4, 5) with sum 9.
+Input: set[] = {3, 34, 4, 12, 5, 2}, sum = 30
+Output: False
+Explanation: There is no subset that adds up to 30.
+
+
+
+
+
+
 /*
 
  0 1 2 3 
@@ -82,4 +98,5 @@ class subset_sum
 		targetSum = 8;
 		subsetSum ( arr, targetSum );
 	}
+
 }
