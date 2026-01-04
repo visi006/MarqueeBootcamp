93a37b8b670193c07a0e705da8dfc130b435badb
Vismay Pradeep <167422286+visi006@users.noreply.github.com>
2026-01-04 12:25:48 +0530

    Implement student class with equals method

--- 11.12.2025/test_student.java
+++ 11.12.2025/test_student.java
@@ -1,1 +1,26 @@
-
+class student
+{
+   String name;
+   int id;
+   student( String name, int id )
+   {
+      this.name = name;
+	  this.id = id;
+   }
+   boolean equals(student gp)
+   {
+	   return name.equals(gp.name) && id == gp.id; 
+   }
+   
+}
+class test_student
+{
+	public static void main( String [] parameters )
+	{
+		student s1 = new student("Jia", 1234);
+		student s2 = new student("Jia", 1234);
+		System.out.println( s1 == s2 );
+		System.out.println( s1.equals(s2) );
+		
+	}
+}
