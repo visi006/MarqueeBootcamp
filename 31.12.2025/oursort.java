9f009331c04e3d8c010515ea2bf49ba149c56ed1
Vismay Pradeep <167422286+visi006@users.noreply.github.com>
2026-01-04 13:32:12 +0530

    Add string sorting algorithm in oursort.java
    Implemented a sorting algorithm to arrange strings in ascending order based on concatenation comparison.

--- 31.12.2025/oursort.java
+++ 31.12.2025/oursort.java
@@ -1,1 +1,31 @@
-
+class oursort
+{
+	public static void main( String [] parameters )
+	{
+		String [] arr = {"3", "30", "34", "5", "9"};
+		int pass_num;
+		int iter_in;
+		String s1s2;
+		String s2s1;
+		String temp;
+		for ( pass_num = 0; pass_num < arr.length - 1; pass_num++)
+		{
+		    for( iter_in = 0; iter_in < arr.length - 1; iter_in++)
+			{
+			   s1s2 = arr [ iter_in ] + arr [ iter_in + 1];
+			   s2s1 = arr [ iter_in + 1] + arr [ iter_in ];
+			   if ( s1s2.compareTo(s2s1) > 0)
+			   {
+			       temp = arr [ iter_in ];
+				   arr [ iter_in ]  = arr [ iter_in + 1];
+				   arr [ iter_in + 1]  = temp;
+			   }
+			}
+		}
+		String res= new String("");;
+		for( int arr_ind = 0; arr_ind < arr.length ; arr_ind++)
+		    res+=arr [ arr_ind ];
+		System.out.println( res );	
+		
+	}
+}
