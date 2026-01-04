0b8684b35250795180b11178c7ea9f89078bc9a2
Vismay Pradeep <167422286+visi006@users.noreply.github.com>
2026-01-04 13:29:41 +0530

    Update cs.py

--- 31.12.2025/cs.py
+++ 31.12.2025/cs.py
@@ -1,1 +1,14 @@
+arr=["3", "30", "34", "5", "9"]
+for pass_num in range ( 0, len(arr)-1):
+  for iter_in in range ( 0, len(arr)- (pass_num+1) ):
+        s1s2 = arr[iter_in] + arr [ iter_in + 1]
+        s2s1 = arr[iter_in+1] + arr [ iter_in ]
+        if s2s1 > s1s2:
+            temp = arr [ iter_in]
+            arr [ iter_in ] = arr [ iter_in +1 ]
+            arr [ iter_in + 1] = temp
+res = "".join(arr)
+print(arr)
+print(res)
+
 
