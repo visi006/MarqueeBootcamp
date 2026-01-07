c5069eec41ef4ff11b130a2a92ac0d5949a3f642
Vismay Pradeep <167422286+visi006@users.noreply.github.com>
2026-01-07 22:20:58 +0530

    Update leetcode313.py

--- 07.01.2026/leetcode313.py
+++ 07.01.2026/leetcode313.py
@@ -1,1 +1,31 @@
+#prime List
+pl = [ 2,7,13,19]
+n = int(input())
+feed_num = 2
+sun_ctr = 1 # super ugly number counter
+while True:
+    num = feed_num
+    #print(num, end =' ' )
+    cf = 2
+    coming_out = False
+    while  num != 1: #4
+         if num % cf == 0: #2
+             if cf not in pl:
+                 coming_out = True
+                 break
+             else:
+                while num % cf == 0 and num != 1:
+                    num//=cf
+         cf+=1
+    if coming_out == False:
+       # print(feed_num, end =  ' ')
+        sun_ctr+=1
+    if sun_ctr == n:
+        print(feed_num)
+        break
+    feed_num+=1
+
+
+
+
 
